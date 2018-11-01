40826d3875d354a5caef38ba0d3075cdbc7909f1
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-01 15:28:19 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,9 @@ GROUP BY pledges.project_id
   ORDER BY projects.title;"
 end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT users.name, pledges.amount FROM users
+LEFT JOIN pledges ON pledges.user_id = users.id
+ORDER BY users.name DESC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
